ec053ede5074c3d98bc757ac669ad788ef8d8f82
Shrut Doshi <shrutvd@gmail.com>
2024-01-16 23:06:26 +0530

    update 15012024

--- routes/web.php
+++ routes/web.php
@@ -63,59 +63,59 @@ Route::group(['prefix' => 'admin', 'namespace' => 'Admin'], function () {
         /**
          * Agent Routes
          */
-        Route::get('/agentadd', [AgentController::class, 'index'])->name('pages.agent');
-        Route::post('agent-create', [AgentController::class, 'AgentCreate'])->name('agent.create');
-        Route::get('agent/edit/{id}', [AgentController::class, 'AgentEdit'])->name('agent.edit');
-        Route::post('/agent-update', [AgentController::class, 'agentUpdate'])->name('agent.update');
-        Route::delete('delete-agenr', [AgentController::class, 'destoryAgent'])->name('destoryAgent');
+        // Route::get('/agentadd', [AgentController::class, 'index'])->name('pages.agent');
+        // Route::post('agent-create', [AgentController::class, 'AgentCreate'])->name('agent.create');
+        // Route::get('agent/edit/{id}', [AgentController::class, 'AgentEdit'])->name('agent.edit');
+        // Route::post('/agent-update', [AgentController::class, 'agentUpdate'])->name('agent.update');
+        // Route::delete('delete-agenr', [AgentController::class, 'destoryAgent'])->name('destoryAgent');
 
         /**
          * Insurance Routes
          */
-        Route::get('/insurance', [InsuranceController::class, 'index'])->name('pages.insurance');
-        Route::post('insurance-create', [InsuranceController::class, 'InsuranceCreate'])->name('insurance.create');
-        Route::get('insurance/edit/{id}', [InsuranceController::class, 'InsuranceEdit'])->name('insurance.edit');
-        Route::post('/insurance-update', [InsuranceController::class, 'InsuranceUpdate'])->name('insurance.update');
-        Route::delete('delete-insurance', [InsuranceController::class, 'destoryInsurance'])->name('destoryInsurance');
+        // Route::get('/insurance', [InsuranceController::class, 'index'])->name('pages.insurance');
+        // Route::post('insurance-create', [InsuranceController::class, 'InsuranceCreate'])->name('insurance.create');
+        // Route::get('insurance/edit/{id}', [InsuranceController::class, 'InsuranceEdit'])->name('insurance.edit');
+        // Route::post('/insurance-update', [InsuranceController::class, 'InsuranceUpdate'])->name('insurance.update');
+        // Route::delete('delete-insurance', [InsuranceController::class, 'destoryInsurance'])->name('destoryInsurance');
 
         /**
          * Discount Routes
          */
-        Route::post('discount-create', [DiscountController::class, 'DiscountCreate'])->name('discount.create');
-        Route::delete('delete-discount', [DiscountController::class, 'destory'])->name('destoryCode');
+        // Route::post('discount-create', [DiscountController::class, 'DiscountCreate'])->name('discount.create');
+        // Route::delete('delete-discount', [DiscountController::class, 'destory'])->name('destoryCode');
 
         /**
          * Supervisor Routes
          */
-        Route::get('/supervisor', [SupervisorController::class, 'index'])->name('pages.supervisor');
-        Route::post('supervisor-create', [SupervisorController::class, 'SupervisorCreate'])->name('supervisor.create');
-        Route::get('supervisor/edit/{id}', [SupervisorController::class, 'supervisorEdit'])->name('supervisor.edit');
-        Route::post('/supervisor-update', [SupervisorController::class, 'supervisorUpdate'])->name('supervisor.update');
-        Route::delete('delete-supervisor', [SupervisorController::class, 'destorysupervisor'])->name('destorysupervisor');
+        // Route::get('/supervisor', [SupervisorController::class, 'index'])->name('pages.supervisor');
+        // Route::post('supervisor-create', [SupervisorController::class, 'SupervisorCreate'])->name('supervisor.create');
+        // Route::get('supervisor/edit/{id}', [SupervisorController::class, 'supervisorEdit'])->name('supervisor.edit');
+        // Route::post('/supervisor-update', [SupervisorController::class, 'supervisorUpdate'])->name('supervisor.update');
+        // Route::delete('delete-supervisor', [SupervisorController::class, 'destorysupervisor'])->name('destorysupervisor');
 
 
         /**
          * Subadmin Routes
          */
-        Route::post('subadmin-create', [SubAdminController::class, 'SubAdminCreate'])->name('subadmin.create');
-        Route::get('subadmin/edit/{id}', [SubAdminController::class, 'SubAdminEdit'])->name('subadmin.edit');
-        Route::post('/subadmin-update', [SubAdminController::class, 'subadminUpdate'])->name('subadminUpdate.update');
-        Route::delete('delete-subadmin', [SubAdminController::class, 'destorySubadmin'])->name('destorySubadmin');
+        // Route::post('subadmin-create', [SubAdminController::class, 'SubAdminCreate'])->name('subadmin.create');
+        // Route::get('subadmin/edit/{id}', [SubAdminController::class, 'SubAdminEdit'])->name('subadmin.edit');
+        // Route::post('/subadmin-update', [SubAdminController::class, 'subadminUpdate'])->name('subadminUpdate.update');
+        // Route::delete('delete-subadmin', [SubAdminController::class, 'destorySubadmin'])->name('destorySubadmin');
         /**
          * Privacy Routes
          */
 
-        Route::get('privacy-form', [PrivacyandPolicyController::class, 'privacyForm'])->name('privacy.form');
-        Route::post('privacy-store', [PrivacyandPolicyController::class, 'privacyStore'])->name('privacy.store');
+        // Route::get('privacy-form', [PrivacyandPolicyController::class, 'privacyForm'])->name('privacy.form');
+        // Route::post('privacy-store', [PrivacyandPolicyController::class, 'privacyStore'])->name('privacy.store');
 
         /**
          * Plan Routes
          */
-        Route::get('/planandrate', [PlanandRateController::class, 'index'])->name('pages.plan');
-        Route::post('plan-create', [PlanandRateController::class, 'PlanCreate'])->name('plan.create');
-        Route::get('plan/edit/{id}', [PlanandRateController::class, 'PlanEdit'])->name('plan.edit');
-        Route::post('/plan-update', [PlanandRateController::class, 'PlanUpdate'])->name('plan.update');
-        Route::delete('delete-plan', [PlanandRateController::class, 'destoryPlan'])->name('destoryPlan');
+        // Route::get('/planandrate', [PlanandRateController::class, 'index'])->name('pages.plan');
+        // Route::post('plan-create', [PlanandRateController::class, 'PlanCreate'])->name('plan.create');
+        // Route::get('plan/edit/{id}', [PlanandRateController::class, 'PlanEdit'])->name('plan.edit');
+        // Route::post('/plan-update', [PlanandRateController::class, 'PlanUpdate'])->name('plan.update');
+        // Route::delete('delete-plan', [PlanandRateController::class, 'destoryPlan'])->name('destoryPlan');
     });
 });
 
